58c574e49c53ba1c2f02f68a5bf979a181696619
Gaoussou Doucoure <gaousd4@gmail.com>
2025-04-09 14:40:37 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void greet(){
+        System.out.println("Hello world. I am in a method/function");
+    } // this void method and will not return anything, mainly used for outputting
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static int add(int a, int b){
+        return a + b;
+    } // this method return to add()
+
+
+    public static String fullName(String first, String last){
+        return first + ", " + last;
+    }
+
+    public static String agePhrase(int age){
+        return  "Your age is" + " " + age;
+    }
+
+    public static void main(String[] args) {
+        greet(); // void method
+        greet(); // void method
+        int sum = add(6, 7); //call/invoke the function above
+        System.out.println(sum);
+        System.out.println(add(11,22));
+        System.out.println(fullName("Gaoussou", "Doucoure"));
+        System.out.println(agePhrase(26));
     }
 }
